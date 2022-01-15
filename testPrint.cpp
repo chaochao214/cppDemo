15d0aaf09d2bb42fb68aae7dcd26bd1b351b5211
田文超 <505530098@qq.com>
2022-01-15 23:42:43 +0800

    cpp

--- testPrint.cpp
+++ testPrint.cpp
@@ -1,9 +1,11 @@
+//
+// Created by tian on 2022/1/15.
+//
+
+#include "testPrint.h"
 #include <iostream>
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
-
-
-
---
+++ testPrint.h
@@ -0,0 +1,14 @@
+//
+// Created by tian on 2022/1/15.
+//
+
+#ifndef MODULO_TESTPRINT_H
+#define MODULO_TESTPRINT_H
+
+
+class testPrint {
+
+};
+
+
+#endif //MODULO_TESTPRINT_H
