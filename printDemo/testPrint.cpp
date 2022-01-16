18190c7ceb5f3422f81251f09e0d2145454ff168
田文超 <505530098@qq.com>
2022-01-16 10:10:25 +0800

    cpp

---
+++ printDemo/testPrint.cpp
@@ -0,0 +1,11 @@
+//
+// Created by tian on 2022/1/15.
+//
+
+#include "testPrint.h"
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World----------------------------------s!" << std::endl;
+    return 0;
+}
